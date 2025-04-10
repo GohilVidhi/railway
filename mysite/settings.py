cbd0b94839d83ca44e684d452c156b8184fa3e17
GohilVidhi <gohilvidhi3@gmail.com>
2025-04-10 17:17:14 +0530

    first commit1

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': "railway",
         'USER': "root",
         'PASSWORD': "PJKhSfdgdQuKeTyCwsllDMnaeSvRAqFA",
